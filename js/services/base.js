6215bb7b001441c348756468e00826379f548bce
WildCat <wildcat.name@gmail.com>
2014-06-18 23:49:31 +0800

    实现基础通信Service的promise

--- js/services/base.js
+++ js/services/base.js
@@ -7,8 +7,8 @@
 
 (function () {
     angular.module('app.services.base', ['ionic', 'toaster', 'app.config'])
-        .service('BaseHttpService', ['$http', '$location', '$ionicLoading', 'toaster', 'appConf', 'errorMessages'
-            , function ($http, $location, $ionicLoading, toaster, appConf, errorMessages) {
+        .service('BaseHttpService', ['$q','$http', '$location', '$ionicLoading', 'toaster', 'appConf', 'errorMessages'
+            , function ($q,$http, $location, $ionicLoading, toaster, appConf, errorMessages) {
                 var BaseHttpService = {
                     _csrf: ''
                 };
@@ -47,16 +47,17 @@
                     }
                 };
 
-                // 虽然不想，但是为了节约时间，写成回调函数了，没有用promise，回头研究 ＝。＝
+                // 已基本实现promise
+                // 但是似乎不是最佳实践
 
                 /**
                  * 基础GET请求
                  * @param uri {String}
                  * @param params {Object}
-                 * @param successCallback {Function}
-                 * @param errorCallback {Function}
+                 * @returns {promise|*}
                  */
-                BaseHttpService.get = function (uri, params, successCallback, errorCallback) {
+                BaseHttpService.get = function (uri, params) {
+                    var deferred = $q.defer();
                     $http({
                         method: 'GET',
                         url: appConf.baseUrl + uri,
@@ -64,23 +65,21 @@
                         timeout: appConf.timeout,
                         responseType: 'json'
                     }).success(function (res) {
-                        successCallback(res);
+                        deferred.resolve(res);
                     }).error(function (error) {
-                        if (errorCallback) {
-                            errorCallback(error);
-                        }
+                        deferred.reject(error);
                     });
+                    return deferred.promise;
                 };
 
                 /**
                  * 基础POST请求
                  * @param uri {String}
                  * @param params {Object}
-                 * @param successCallback {Function}
-                 * @param errorCallback {Function}
+                 * @returns {promise|*}
                  */
-                BaseHttpService.post = function (uri, params, successCallback, errorCallback) {
-//                    params._csrf = this._csrf;
+                BaseHttpService.post = function (uri, params) {
+                    var deferred = $q.defer();
                     $http({
                         method: 'POST',
                         url: appConf.baseUrl + uri,
@@ -89,78 +88,70 @@
                         responseType: 'json',
                         headers: {'Content-Type': 'application/x-www-form-urlencoded', 'x-csrf-token': this._csrf}
                     }).success(function (res) {
-                        successCallback(res);
+                        deferred.resolve(res);
                     }).error(function (error) {
-                        if (errorCallback) {
-                            errorCallback(error);
-                        }
+                        deferred.reject(error);
                     });
+                    return deferred.promise;
                 };
 
                 /**
                  * GET: 附加UI响应(包含错误响应)
                  * @param uri {String}
                  * @param params {Object}
-                 * @param successCallback {Function}
-                 * @param errorCallback {Function}
+                 * @returns {promise|*}
                  */
-                BaseHttpService.getWithUi = function (uri, params, successCallback, errorCallback) {
+                BaseHttpService.getWithUi = function (uri, params) {
+                    var deferred = $q.defer();
                     uiLoading.show();
                     this.get(uri, params, function (res) {
                         uiLoading.hide();
                         // 请求成功， 但是需求判断status， 判定操作是否成功
                         if (res.status === 1) {
-                            successCallback(res.data);
+                            deferred.resolve(res.data);
                         } else {
                             _errorHandler(res);
-
-                            if (errorCallback) {
-                                errorCallback(res);
-                            }
+                            deferred.reject(res);
                         }
                     }, function (error) {
                         uiLoading.hide();
                         // 请求失败， 为网络错误
                         toaster.pop('error', '网络错误', '请求失败');
-                        if (errorCallback) {
-                            errorCallback(error);
-                        }
+                        deferred.reject(error);
                     });
+                    return deferred.promise;
                 };
 
                 /**
                  * POST: 附加UI响应(包含错误响应)
                  * @param uri {String}
                  * @param params {Object}
-                 * @param successCallback {Function}
-                 * @param errorCallback {Function}
+                 * @returns {promise|*}
                  */
-                BaseHttpService.postWithUi = function (uri, params, successCallback, errorCallback) {
+                BaseHttpService.postWithUi = function (uri, params) {
+                    var deferred = $q.defer();
                     uiLoading.show();
                     this.post(uri, params, function (res) {
                         uiLoading.hide();
                         // 请求成功， 但是需求判断status， 判定操作是否成功
                         if (res.status === 1) {
-                            successCallback(res.data);
+                            deferred.resolve(res.data);
                         } else {
                             _errorHandler(res);
 
-                            if (errorCallback) {
-                                errorCallback(res);
-                            }
+                            deferred.reject(res);
                         }
                     }, function (error) {
                         uiLoading.hide();
                         // 请求失败， 为网络错误
                         toaster.pop('error', '网络错误', '请求失败');
-                        if (errorCallback) {
-                            errorCallback(error);
-                        }
+                        deferred.reject(error);
                     });
+                    return deferred.promise;
                 };
 
                 BaseHttpService.getCsrf = function () {
-                    this.get('/get_csrf', null, function (res) {
+                    this.get('/get_csrf', null).then(function (res) {
                         if (res.status === 1 && res.data.csrf) {
                             BaseHttpService._csrf = res.data.csrf;
                         }
