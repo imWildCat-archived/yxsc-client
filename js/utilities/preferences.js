ba76a1e7e0de516ccd2dd9da847c943e698d8ffd
WildCat <wildcat.name@gmail.com>
2014-06-18 23:15:15 +0800

    添加用户配置实现

---
+++ js/utilities/preferences.js
@@ -0,0 +1,55 @@
+/**
+ * Preferences - Preferences.*
+ * 用户配置
+ *
+ * @author WildCat <wildcat.name@gmail.com>
+ * @date 2014-06-18
+ */
+(function () {
+    angular.module('app.utilities.Preferences', ['ngStorage']).service('Preferences', ['$localStorage', function ($localStorage) {
+        var Preferences = {};
+
+        /**
+         * 获取配置
+         * @param key {String}
+         * @returns {*}
+         */
+        Preferences.get = function (key) {
+            return $localStorage[key];
+        };
+
+
+        /**
+         * 设定配置
+         * @param key {String}
+         * @param value {String}
+         * @returns {*}
+         */
+        Preferences.set = function (key, value) {
+            return $localStorage[key] = value;
+        };
+
+        /**
+         * 存储用户登录信息
+         * @param username
+         * @param password
+         * @returns {*}
+         */
+        Preferences.saveLoginInfo = function (username, password) {
+            return Preferences.set('saved_user', {
+                username: username,
+                password: password
+            });
+        };
+
+        /**
+         * 获取用户登录信息
+         * @returns {*}
+         */
+        Preferences.getLoginInfo = function () {
+            return Preferences.get('saved_user');
+        };
+
+        return Preferences;
+    }]);
+})();
