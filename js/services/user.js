6392f9ac4d2bd4b702e3c02c5e4939ab76f298be
WildCat <wildcat.name@gmail.com>
2014-06-18 23:33:32 +0800

    新增检测用户是否登录的方法

--- js/services/user.js
+++ js/services/user.js
@@ -5,14 +5,35 @@
  * @date 2014-06-13
  */
 
+// 需要注意的是，UserService 不仅仅是数据通信类，而且在内存中缓存着用户的信息
+
 (function () {
-    angular.module('app.services.User', ['app.services.base']).service('UserService', ['BaseHttpService', function (BaseHttpService) {
+    angular.module('app.services.User', ['app.services.base', 'toaster']).service('UserService', ['BaseHttpService', '$location', 'toaster', function (BaseHttpService, $location, toaster) {
         var UserService = {
-            currentUser: null // 保存当前用户数据
+            currentUser: null, // 保存当前用户数据
+
+            // 客户端数据
+            clientData : {
+                currentCampus : null // 当前校区
+            }
         };
 
         UserService.getCsrf = function () {
             BaseHttpService.getCsrf();
+        };
+
+        /**
+         * 检查是否登录
+         * @param message {String} 未登录时的提醒消息
+         * @returns {boolean}
+         */
+        UserService.checkLogin = function (message) {
+            if (!UserService.currentUser) {
+                toaster.pop('warning', '您尚未登录，请登录', message);
+                $location.path('/user/login');
+                return false;
+            }
+            return true;
         };
 
         /**
@@ -39,7 +60,7 @@
             })
         };
 
-        UserService.logout = function(successCallback){
+        UserService.logout = function (successCallback) {
 
         };
 
