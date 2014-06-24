50811a5cd83e36f717e64c4f154428e5363c9e16
WildCat <wildcat.name@gmail.com>
2014-06-24 23:18:03 +0800

    实现图片上传模块

---
+++ js/services/image.js
@@ -0,0 +1,77 @@
+/**
+ * ImageService - ImageService.*
+ *
+ * @author WildCat <wildcat.name@gmail.com>
+ * @date 2014-06-23
+ */
+
+(function () {
+    angular.module('app.services.Image', ['app.services.base', 'toaster', 'angularFileUpload', 'app.config']).service('ImageService', ['$q', '$http', 'BaseHttpService', 'appConf', '$location', 'toaster', '$upload', function ($q, $http, BaseHttpService, appConf, $location, toaster, $upload) {
+        var ImageService = {
+            token: null
+        };
+
+        ImageService.getToken = function () {
+            BaseHttpService.get('/get_img_token', null).then(function (res) {
+                ImageService.token = res.data.token;
+            });
+        };
+
+        ImageService.postImg = function (file) {
+            var formData = new FormData();
+            formData.append('file', file);
+
+            var deferred = $q.defer();
+            $http({
+                method: 'POST',
+                url: 'http://api.tietuku.com/v1/Up',
+                params: {Token: ImageService.token},
+                data: formData,
+                timeout: appConf.timeout,
+//                responseType: 'json',
+                headers: {'Content-Type': 'multipart/form-data'}
+            }).success(function (res) {
+                deferred.resolve(res);
+            }).error(function (error) {
+                deferred.reject(error);
+            });
+            return deferred.promise;
+        };
+
+        ImageService.initImageService = function ($scope, successCallBack, errorCallback) {
+            $scope.onFileSelect = function ($files) {
+                //$files: an array of files selected, each file has name, size, and type.
+                for (var i = 0; i < $files.length; i++) {
+                    toaster.pop('info', '上传中...');
+                    var file = $files[i];
+                    $scope.upload = $upload.upload({
+                        url: 'http://api.tietuku.com/v1/Up', //upload.php script, node.js route, or servlet url
+                        // method: 'POST' or 'PUT',
+                        // headers: {'header-key': 'header-value'},
+                        // withCredentials: true,
+                        data: {Token: ImageService.token},
+                        file: file // or list of files: $files for html5 only
+                        /* set the file formData name ('Content-Desposition'). Default is 'file' */
+                        //fileFormDataName: myFile, //or a list of names for multiple files (html5).
+                        /* customize how data is added to formData. See #40#issuecomment-28612000 for sample code */
+                        //formDataAppender: function(formData, key, val){}
+                    }).progress(function (evt) {
+//                        console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
+                    }).success(function (data, status, headers, config) {
+                        // file is uploaded successfully
+                        successCallBack(data, status, headers, config);
+                    }).error(function (error) {
+                        errorCallback(error);
+                    });
+                }
+            };
+        }
+        ;
+
+        return ImageService;
+
+    }
+    ])
+    ;
+})
+();
